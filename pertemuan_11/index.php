3208d091d1ffc9d98790896c5c4aa046d413aaae
Sasmita <tata.wiropati@gmail.com>
2024-12-06 09:01:53 +0700

    update p11 index

--- pertemuan_11/index.php
+++ pertemuan_11/index.php
@@ -14,21 +14,13 @@
         }
     </style>
 </head>
-<<<<<<< HEAD
 <body class="d-flex flex-column align-items-center">
-=======
-<body class="d-flex align-items-center">
->>>>>>> 6e359cd7702462939d55a7e644480e92c657eebb
     <main class="m-auto p-4 rounded shadow">
         <p class="h5">Pendaftaran Siswa Baru</p>
         <p class="h1">SMK Cerdas</p>
         <div class="py-2">
             <a href="form-daftar.php" class="btn btn-primary">Daftar Sekarang</a>
-<<<<<<< HEAD
             <a href="list-siswa.php" class="btn btn-outline-primary">List Pendaftar</a>
-=======
-            <a href="list-siswa.php" class="btn btn-outline-secondary">List Pendaftar</a>
->>>>>>> 6e359cd7702462939d55a7e644480e92c657eebb
         </div>
         <?php if(isset($_GET['status'])): ?>
         <p>
@@ -42,12 +34,9 @@
         </p>
         <?php endif; ?>
     </main>
-<<<<<<< HEAD
     <div class="py-2 position-fixed fixed-bottom">
         <p class="h6">Butuh Bantuan?</p>
         <a href="list-pegawai.php" class="btn btn-secondary">Hubungi Kami</a>
     </div>
-=======
->>>>>>> 6e359cd7702462939d55a7e644480e92c657eebb
 </body>
 </html>
